255f0cbecc712a4bad2775b362abb901c3eaa3ec
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-05-02 22:15:46 +0800

    Upload Include File
    File from Include

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>2022-2023</b>
+    </div>
+    <strong>PC Builder Website 2022 By Aqil Nu'man RC21299</strong>
+</footer>
