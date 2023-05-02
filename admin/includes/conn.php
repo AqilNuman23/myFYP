a6cde954695d3a1f76fffe8eef3ca5049e6e6c50
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-05-02 22:44:47 +0800

    upload /admin/include

---
+++ admin/includes/conn.php
@@ -0,0 +1,30 @@
+<?php
+
+Class Database{
+ 
+	private $server = "mysql:host=10.26.30.17;dbname=rc21299";
+	private $username = "rc21299";
+	private $password = "rc21299";
+	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+	protected $conn;
+ 	
+	public function open(){
+ 		try{
+ 			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+ 			return $this->conn;
+ 		}
+ 		catch (PDOException $e){
+ 			echo "There is some problem in connection: " . $e->getMessage();
+ 		}
+ 
+    }
+ 
+	public function close(){
+   		$this->conn = null;
+ 	}
+ 
+}
+
+$pdo = new Database();
+ 
+?>
---
+++ admin/includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>2022-2023 </b>
+      </div>
+      <strong>PC Builder Website 2022 By Aqil Nu'man RC21299</strong>
+    </div>
+</footer>
